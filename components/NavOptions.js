6d8b1abf128398b16c7520cd8bed13f7c300e8ae
piyush97 <me@piyushmehta.com>
2022-05-15 17:50:34 -0400

    rn elements

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -8,15 +8,16 @@ const NavOptions = () => {
       style={tw`px-2`}
       data={menuData}
       horizontal
+      scrollEnabled={false}
       keyExtractor={(item) => item.id.toString()}
       renderItem={({ item }) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200`}>
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 m-2 w-40`}>
           <View>
             <Image
               source={{ uri: item.image }}
-              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              style={{ width: 100, height: 100, resizeMode: "contain" }}
             />
-            <Text>{item.title}</Text>
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
           </View>
         </TouchableOpacity>
       )}
