3478de03994f78682bde1e0b7d4cb5a7f4bb50fb
piyush97 <me@piyushmehta.com>
2022-05-15 13:36:55 -0400

    installed redux toolkit

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Pool it up</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
