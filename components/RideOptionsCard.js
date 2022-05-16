518108eb716274186da2c0c188fa255f9d727e3e
piyush97 <me@piyushmehta.com>
2022-05-16 18:38:07 -0400

    Greeting logic

---
+++ components/RideOptionsCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import tw from "twrnc";
+const RideOptionsCard = () => {
+  return (
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>
+        {new Date().getHours() < 12
+          ? "Good Morning"
+          : new Date().getHours() > 12 && new Date().getHours() < 18
+          ? "Good Afternoon"
+          : "Good Evening"}
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default RideOptionsCard;
