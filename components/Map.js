cb8bd5d058496f9d20786eebad998f457ace578d
piyush97 <me@piyushmehta.com>
2022-05-16 17:03:24 -0400

    fixes

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,5 @@
 import React from "react";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import { useSelector } from "react-redux";
 import tw from "twrnc";
 import { selectOrigin } from "../slices/navSlice";
@@ -11,11 +11,20 @@ const Map = () => {
       initialRegion={{
         latitude: origin.location.lat,
         longitude: origin.location.lng,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
       }}
       mapType="mutedStandard"
-    />
+    >
+      {origin?.location && (
+        <Marker
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+          }}
+        />
+      )}
+    </MapView>
   );
 };
 
