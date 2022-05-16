1d8406bb2d794220b6c031cf6dec6cb59cab80a7
piyush97 <me@piyushmehta.com>
2022-05-16 19:04:56 -0400

    safe

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -3,7 +3,9 @@ import { SafeAreaView, Text } from "react-native";
 import tw from "twrnc";
 const RideOptionsCard = () => {
   return (
-    <SafeAreaView style={tw`bg-white flex-1`}>
+    <SafeAreaView
+      style={tw`bg-white flex-1 border-t border-gray-200 flex-shrink`}
+    >
       <Text style={tw`text-center py-5 text-xl`}>
         {new Date().getHours() < 12
           ? "Good Morning"
