2ebe94b04f3ea0eec9d0a509e7c1daf6e87b5a3c
piyush97 <me@piyushmehta.com>
2022-05-17 17:47:50 -0400

    rideoptions

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,6 +1,13 @@
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { useDispatch } from "react-redux";
 import tw from "twrnc";
@@ -9,7 +16,26 @@ import NavFavourites from "./NavFavourites";
 
 const RideOptionsCard = () => {
   const dispatch = useDispatch();
-
+  const data = [
+    {
+      id: "1",
+      title: "Ride from Piyush",
+      price: 45,
+      carType: "Sedan",
+      image: require("../assets/SEDAN.webp"),
+      carModel: "Honda Civic",
+      availableSeats: 4,
+    },
+    {
+      id: "2",
+      title: "Ride from John",
+      price: 55,
+      carType: "SUV",
+      image: require("../assets/SUV.webp"),
+      carModel: "Honda CRV",
+      availableSeats: 3,
+    },
+  ]; // TODO: get data from API
   return (
     <SafeAreaView
       style={tw`bg-white flex-1 border-t border-gray-200 flex-shrink`}
@@ -54,6 +80,42 @@ const RideOptionsCard = () => {
         placeholder="Where to?"
       />
       <NavFavourites />
+      <View>
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({
+            item: {
+              id,
+              title,
+              carType,
+              price,
+              availableSeats,
+              image,
+              carModel,
+            },
+          }) => (
+            <TouchableOpacity onPress={() => {}}>
+              <View style={tw`flex-row bg-white border-b border-gray-200 p-3`}>
+                <View style={tw`flex-1`}>
+                  <Text style={tw`text-xl`}>{title}</Text>
+                  <Text style={tw`text-gray-600`}>{carType}</Text>
+                  <Text style={tw`text-gray-600 `}>{carModel}</Text>
+                  <Text style={tw`text-gray-600 font-semibold`}>
+                    {availableSeats} seats
+                  </Text>
+                </View>
+                <View style={tw`flex-1`}>
+                  <Text style={tw`text-xl`}>${price}</Text>
+                </View>
+                <View style={tw`flex-1`}>
+                  <Image source={image} style={tw`h-32 w-35`} />
+                </View>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 };
