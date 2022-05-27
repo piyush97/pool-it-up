b803c27e61964c0730ee4d593e6b358a6bcd3ce4
piyush97 <me@piyushmehta.com>
2022-05-27 00:50:18 -0400

    Fixes

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,4 +1,4 @@
-import { Icon, Image, Text } from '@rneui/themed';
+import { AirbnbRating, Icon, Image, Text } from '@rneui/themed';
 import React from 'react';
 import { SafeAreaView, TouchableOpacity } from 'react-native';
 import tw from 'twrnc';
@@ -28,65 +28,123 @@ const RideCard = ({
         selected && selected?.id === id
           ? {
               backgroundColor: theme.colors.primary,
-              ...tw`h-22  p-2 border-b border-black`,
+              ...tw`h-25  p-2 border-b border-black`,
             }
-          : { ...tw`h-22  p-2 border-b border-black` }
+          : { ...tw`h-25  p-2 border-b border-black` }
       }
     >
-      <Image source={carType?.toLowerCase() === 'sedan' ? SEDAN : SUV} style={tw`w-20 h-12 `} />
-      <Text style={tw`text-sm text-left ml-4 mt-1 `}>{carModel}</Text>
+      <AirbnbRating
+        starContainerStyle={{
+          position: 'absolute',
+          top: '1%',
+          right: '2%',
+          bottom: 'auto',
+        }}
+        count={5}
+        defaultRating={3}
+        size={10}
+        isDisabled={true}
+        showRating={false}
+        selectedColor={selected && selected?.id !== id ? theme.colors.primary : theme.colors.black}
+      />
+      <Image
+        source={carType?.toLowerCase() === 'sedan' ? SEDAN : SUV}
+        style={tw`w-20 h-18 mt-3 `}
+      />
+      <Text
+        style={{
+          position: 'absolute',
+          top: '1%',
+          left: 'auto',
+          right: 'auto',
+          alignContent: 'center',
+          ...tw`text-xl font-semibold text-left ml-4 mt-1 `,
+        }}
+      >
+        {carModel}
+      </Text>
       <SafeAreaView
         style={{
           position: 'absolute',
-          top: '50%',
+          top: '75%',
           left: 'auto',
-          right: '50%',
+          right: '45%',
           alignContent: 'center',
         }}
       >
+        <Text
+          style={{
+            position: 'absolute',
+            top: '20%',
+            left: '130%',
+            right: 'auto',
+            alignContent: 'center',
+            textAlign: 'center',
+            ...tw`text-sm  font-semibold`,
+          }}
+        >
+          {availableSeats}
+        </Text>
         <Icon
           type="font-awesome"
           name="user"
           color={selected && selected?.id !== id ? theme.colors.grey3 : theme.colors.black}
         />
-        <Text style={{ textAlign: 'center', ...tw`text-sm mt-1 font-semibold` }}>
-          {availableSeats}
-        </Text>
       </SafeAreaView>
+      <Text
+        style={{
+          position: 'absolute',
+          top: '10%',
+          left: 'auto',
+          right: '76%',
+          color: theme.colors.grey1,
+          ...tw`text-md  text-left font-light`,
+        }}
+      >
+        {carType}
+      </Text>
       <SafeAreaView
         style={{
           position: 'absolute',
-          top: '50%',
+          top: '75%',
           left: 'auto',
           right: '60%',
           alignContent: 'center',
         }}
       >
+        <Text
+          style={{
+            position: 'absolute',
+            top: '20%',
+            left: '130%',
+            right: 'auto',
+            alignContent: 'center',
+            textAlign: 'center',
+            ...tw`text-sm  font-semibold`,
+          }}
+        >
+          {bags_available}
+        </Text>
         <Icon
           type="font-awesome"
           name="suitcase"
           color={selected && selected?.id !== id ? theme.colors.grey3 : theme.colors.black}
         />
-        <Text style={{ textAlign: 'center', ...tw`text-sm mt-1 font-semibold` }}>
-          {bags_available}
-        </Text>
       </SafeAreaView>
-      {/* <Icon type="font-awesome" name="suitcase" style={{}} /> */}
-
       <Text
         style={
           selected?.id !== id
             ? {
                 position: 'absolute',
                 right: '5%',
-                top: '40%',
+                top: '50%',
                 color: theme.colors.primary,
                 ...tw`text-xl text-right font-bold`,
               }
             : {
                 position: 'absolute',
                 right: '5%',
-                top: '40%',
+                top: '50%',
                 color: theme.colors.black,
                 ...tw`text-xl text-right font-bold`,
               }
