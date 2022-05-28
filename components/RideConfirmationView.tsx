c058600a9e2b1c6670e7f15a3880192f85dd1d05
piyush97 <me@piyushmehta.com>
2022-05-27 20:41:39 -0400

    fixing

--- components/RideConfirmationView.tsx
+++ components/RideConfirmationView.tsx
@@ -1,11 +1,12 @@
 import { Button, Text, useTheme } from '@rneui/themed';
 import React, { useEffect } from 'react';
-import { Image, SafeAreaView } from 'react-native';
+import { Image, Modal, SafeAreaView } from 'react-native';
 import tw from 'twrnc';
 import { carImageProvider } from '../constants/fetchDetails';
 import dbService from '../service/DbService';
 import { RideConfirmationViewProps } from '../types/env';
 import FromTo from './FromTo';
+import StripePaymentView from './StripePaymentView';
 
 /**
  * @description - Ride Confirmation View
@@ -17,6 +18,7 @@ const RideConfirmationView = ({ selected }: RideConfirmationViewProps) => {
   const { theme } = useTheme();
   const { getRideData } = dbService;
   const [rideDetails, setRideDetails] = React.useState<any>();
+  const [showModal, setShowModal] = React.useState(false);
   useEffect(() => {
     const getRideDataDetails = async () => {
       const data = await getRideData('a1879470-dbb6-451e-941d-0065e74614ce'); //TODO: remove this hardcoded id [only for testing]
@@ -37,7 +39,23 @@ const RideConfirmationView = ({ selected }: RideConfirmationViewProps) => {
       />
       <FromTo from={rideDetails?.from?.description} to={rideDetails?.to?.description} />
       {/* <Text>{JSON.stringify(rideDetails, null, 4)}</Text> */}
-      <Button title="Confirm" style={{ ...tw`p-3 pt-5` }} onPress={() => {}} />
+      <Button
+        title="Confirm"
+        style={{ ...tw`p-3 pt-5` }}
+        onPress={() => {
+          setShowModal(true);
+        }}
+      />
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={showModal}
+        onRequestClose={() => {
+          setShowModal(false);
+        }}
+      >
+        <StripePaymentView email={'me@piyushmehta.com'} />
+      </Modal>
     </SafeAreaView>
   );
 };
