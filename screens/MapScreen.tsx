efa8a2d2e9194de2d6f141f1c3ce34a31946c591
piyush97 <me@piyushmehta.com>
2022-07-09 19:01:17 -0400

    map screen fixing

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,16 +1,22 @@
 import BottomSheet from '@gorhom/bottom-sheet';
 import { useTheme } from '@rneui/themed';
-import React, { useCallback, useMemo, useRef } from 'react';
+import React, { useMemo, useRef } from 'react';
 import { View } from 'react-native';
 import tw from 'twrnc';
 import Map from '../components/Map';
 import RideOptionsCard from '../components/RideOptionsCard';
-function MapScreen() {
+
+/**
+ * Map Screen Component - Map
+ *
+ *
+ * @return {React.ReactElement} - The Map Screen Component
+ * @author - Piyush Mehta <me@piyushmehta.com>
+ */
+const MapScreen = () => {
   const bottomSheetRef = useRef<BottomSheet>(null);
   const snapPoints = useMemo(() => ['50%', '50%', '85%'], []);
-  const handleSheetChanges = useCallback((index: number) => {
-    console.log('handleSheetChanges', index);
-  }, []);
+
   const { theme } = useTheme();
   return (
     <View style={{ height: '100%', backgroundColor: theme.colors.background }}>
@@ -25,12 +31,11 @@ function MapScreen() {
         handleIndicatorStyle={{ backgroundColor: theme.colors.black }}
         detached={true}
         snapPoints={snapPoints}
-        onChange={handleSheetChanges}
       >
         <RideOptionsCard />
       </BottomSheet>
     </View>
   );
-}
+};
 
 export default MapScreen;
